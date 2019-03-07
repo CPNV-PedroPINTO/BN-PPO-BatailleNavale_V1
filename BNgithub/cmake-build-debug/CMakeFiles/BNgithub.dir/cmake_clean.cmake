54e66ac4e5b8486e9829e6231a6d232dbecc9407
PedroPINTODjDj <47849654+PedroPINTODjDj@users.noreply.github.com>
2019-03-07 15:50:24 +0100

    BN-Hellor world

---
+++ BNgithub/cmake-build-debug/CMakeFiles/BNgithub.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BNgithub.dir/main.c.obj"
+  "BNgithub.pdb"
+  "BNgithub.exe"
+  "BNgithub.exe.manifest"
+  "libBNgithub.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BNgithub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
